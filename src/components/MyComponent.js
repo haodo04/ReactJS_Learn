db73d9c653e7e391bb8fa741d4ecee5b8fff5987
haodo04 <22130077@st.hcmuaf.edu.vn>
2025-09-25 02:48:40 +0700

    fragment in react

--- src/components/MyComponent.js
+++ src/components/MyComponent.js
@@ -3,30 +3,30 @@ import AddUserInfo from "./AddUserInfo";
 import DisplayInfo from "./DisplayInfo";
 
 class MyComponent extends React.Component {
-
   state = {
     listUsers: [
-      {id: 1, name: "Hao do", age: "30"},
-      {id: 2, name: "Tuan", age: "20"},
-      {id: 3, name: "Son", age: "24"}
-    ]
-  }
+      { id: 1, name: "Hao do", age: "30" },
+      { id: 2, name: "Tuan", age: "20" },
+      { id: 3, name: "Son", age: "24" },
+    ],
+  };
 
   handleAddNewUser = (userObject) => {
-    console.log(">>>> check data from parent: ", userObject)
-  }
+    console.log(">>>> check data from parent: ", userObject);
+  };
 
   render() {
     return (
-      <div>
-        <AddUserInfo
-          handleAddNewUser={this.handleAddNewUser}
-        />
-        <br/> <br/>
-        <DisplayInfo 
-        listUsers={this.state.listUsers}
-        />
-      </div>
+      <>
+        <div>
+          <div className="a">
+            <AddUserInfo handleAddNewUser={this.handleAddNewUser} />
+            <br /> <br />
+            <DisplayInfo listUsers={this.state.listUsers} />
+          </div>
+          <div className="b"></div>
+        </div>
+      </>
     );
   }
 }
