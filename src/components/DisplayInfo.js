9e26585c6ebf49c1725c6a803b063e7fdfce15e4
haodo04 <22130077@st.hcmuaf.edu.vn>
2025-09-25 02:40:31 +0700

    use image in react

--- src/components/DisplayInfo.js
+++ src/components/DisplayInfo.js
@@ -1,5 +1,6 @@
 import React from "react";
 import "./DisplayInfo.scss"
+import logo from '../logo.svg'
 
 class DisplayInfo extends React.Component {
 
@@ -18,6 +19,7 @@ class DisplayInfo extends React.Component {
     const { listUsers } = this.props; // object
     return (
       <div className="display-info-container">
+        <img src={logo}/>
         <div>
             <span onClick={() => {this.handleShowHide()}}>Hide list user</span>
 
